42cfc7a8c99d93b9d04d13d91d67628accd5b874
Haseeb <haseeb.emailbox@gmail.com>
2017-08-20 23:56:21 +0500

    Word Tagging Done

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import MyClass
+import nltk
+from nltk.corpus import treebank
+from nltk.corpus import alpino as alp
+from nltk.tag import UnigramTagger, BigramTagger
+
 
 # Create your views here.
 
 def index(request):
-	context = {}
+	if request.method == "POST":
+		text =  request.POST.get('input')
+		tokens = nltk.word_tokenize(text)
+		tagged = nltk.pos_tag(tokens)
+		context = { 'input': tagged}
+
+	else:
+		context = { 'input': "No Data" }
+		
 	return render(request,'mainapp/index.html',context)
 
 
